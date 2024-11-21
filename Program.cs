36e4ea9338a27d4f3c1faab7d3dc3e129d37bdad
FridaUltra <5carlsonfrida@gmail.com>
2024-11-21 16:09:41 +0100

    Welcome() WIP

--- Program.cs
+++ Program.cs
@@ -6,33 +6,12 @@ internal class Program
     private static void Main(string[] args)
     {
         var db = new DatabaseRepo();
+        Menu<string> menu = new();
         Menu<Competition> competitionsMenu = new();
-        var ui = new UserInterface(db, competitionsMenu);
+        Menu<Dog> dogsMenu = new();
+        var ui = new UserInterface(db, competitionsMenu, menu, dogsMenu);
 
-        // var dog = ui.PickDog();
-		// ui.DisplayDogAverageScore(dog!);
-
-		// var competition = ui.ChooseCompetitionByEnteringID();
-        
-		// var highscoreList = db.GetHighscoreForCompetition(competition);
-
-		// foreach (var item in highscoreList)
-		// {
-		// 	Console.WriteLine($"Hund: {item.Dog} \t Ras: {item.Breed} \t Poäng: {item.Points} \t Ägare: {item.Owner}");
-		// }
-
-
-    //     List<Competition> menuOptions = db.GetAllCompetitions();
-    //    Menu<Competition> competitionsMenu2 = new(); 
-    //    var competition2 = competitionsMenu.Display(menuOptions);
-    //     Console.WriteLine(competition2.Id);
-
-        // injecta menyn i ui:t och lägg in i nån metod där.
-
-        //Hämtar alla tävlingar och visar en menu med pilar. Id på den valda returneras.
-        var competition = ui.ChooseCompetition();
-        Console.WriteLine(competition);
-
+        ui.Welcome();
 
     }
 }
--- View/UserInterface.cs
+++ View/UserInterface.cs
@@ -2,11 +2,83 @@ using Model;
 
 namespace View;
 
-public class UserInterface(DatabaseRepo db, Menu<Competition> competitionsMenu)
+public class UserInterface(DatabaseRepo db, Menu<Competition> competitionsMenu, Menu<string> menu, Menu<Dog> dogsMenu)
 {
   private readonly DatabaseRepo _db = db;
   private readonly Menu<Competition> _competitionsMenu = competitionsMenu;
+  private readonly Menu<string> _menu = menu;
+  private readonly Menu<Dog> _dogsMenu = dogsMenu;
 
+  public void Welcome()
+  {
+    while (true)
+    {
+      Console.ForegroundColor = ConsoleColor.DarkCyan;
+      Console.WriteLine("Välkommen!\n");
+      Console.WriteLine("Här kan du se resultat och genomsnittsbetyg för alla hundar och tävlingar");
+      Console.ForegroundColor = ConsoleColor.White;
+      List<string> menuoptions = new List<string>{"Highscore för en tävling", "Highscoore för alla tävlingar", "Genomsnittsbetyg för en hund"};
+      string header = "Huvudmenu";  
+      var retur = _menu.Display(menuoptions, header);
+
+      
+      if (retur == "Highscore för en tävling")
+      {
+        var competition = ChooseCompetition();
+        List<HighscoreEntry> highscores = _db.GetHighscoreForCompetition(competition);
+
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine(competition.Name +"\n");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        foreach (var item in highscores)
+        {
+          Console.WriteLine($"{item.Dog},\tPoäng: {item.Points},\tÄgare: {item.Owner},\t Ras: {item.Breed} ");
+        }
+
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Vill du återgå till menyn? y/n");
+        Console.ForegroundColor = ConsoleColor.White;
+        string svar = Console.ReadLine().ToLower();
+        if(svar == "y") continue;
+        else if(svar == "n") break;
+      }
+      else if(retur == "Highscoore för alla tävlingar")
+      {
+
+      }
+      else if(retur == "Genomsnittsbetyg för en hund")
+      {
+        while (true)
+        {
+          // Hämta ut alla hundar.
+          List<Dog> menuOptions = _db.GetDogs();
+          header = "Välj den hund du vill se genomsnittsbetyg för";
+          var dog = _dogsMenu.Display(menuOptions, header);
+          var averageScore = _db.GetDogAverageScore(dog);
+
+          Console.WriteLine($"Genomsnittsbetyget för {dog.Name} är {averageScore}");
+          Thread.Sleep(2000);
+
+          Console.WriteLine();
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("Vill du se för någon annan hund? y/n");
+          Console.ForegroundColor = ConsoleColor.White;
+          string svar = Console.ReadLine().ToLower();
+          if(svar == "y") continue;
+          else if(svar == "n") break;
+          
+        }
+      }
+      else{
+        break;
+      }
+    }
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine("Tack och välkommen åter");
+    Console.ForegroundColor = ConsoleColor.White;
+  }
   
   public Dog? PickDog()
   {
@@ -51,9 +123,11 @@ public class UserInterface(DatabaseRepo db, Menu<Competition> competitionsMenu)
 
   public Competition ChooseCompetition()
   {
+    // Hämtar alla tävlingar
     List<Competition> menuOptions = _db.GetAllCompetitions();
-    var competition = competitionsMenu.Display(menuOptions);
-        Console.WriteLine(competition.Id);
+    string header = "Alla tävlingar";
+    //Visar menu och returnerar den valda tävlingen.
+    var competition = _competitionsMenu.Display(menuOptions, header);
     return competition;    
   }
 
